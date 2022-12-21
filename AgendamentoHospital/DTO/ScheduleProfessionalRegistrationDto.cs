9ac54396332aad7fe2fd9ff9dc82da40449abe98
Huly Marianne <h.santos.das.chagas@avanade.com>
2022-12-21 13:55:49 -0300

    Implementação da api dos profissionais

---
+++ AgendamentoHospital/DTO/ScheduleProfessionalRegistrationDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AgendamentoHospital.DTO
+{
+    public class ScheduleProfessionalRegistrationDto
+    {
+        public int IdProfessional { get; set; }
+
+        [Required(ErrorMessage = "Informe o nome")]
+        public string Name { get; set; }
+
+        //[RegularExpression("^(?!0+$)(\\+\\d{1,3}[- ]?)?(?!0+$)\\d{10,15}$", ErrorMessage = "Por favor insira um número válido!")]
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public bool Active { get; set; }
+      
+    }
+}
