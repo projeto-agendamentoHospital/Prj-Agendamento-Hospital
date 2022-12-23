e821341cc6ae6319bc42be1781911ac1c0dc1efd
Vitoria Neris <v.a.mendes.da.silva@avanade.com>
2022-12-22 22:11:59 -0300

    API Agendamento Finalizada

---
+++ AgendamentoHospital/Controllers/ScheduleController.cs
@@ -0,0 +1,95 @@
+﻿using AgendamentoHospital.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgendamentoHospital.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ScheduleController : Controller
+    {
+        private readonly ScheduleRepository scheduleRepository;
+
+        public ScheduleController()
+        {
+            scheduleRepository = new ScheduleRepository();
+        }
+
+        [HttpGet]
+        public IActionResult Query()
+        {
+            try
+            {
+                var listSchedule = scheduleRepository.ListingSchedules();
+                return Ok(listSchedule);
+            }
+            catch (KeyNotFoundException)
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpGet]
+        [Route("Query/{id}")]
+        public ActionResult Query(int id)
+        {
+            try
+            {
+                var listSchedule = scheduleRepository.Query(id);
+                if (listSchedule.IdSchedule != 0)
+                {
+                    return Ok(listSchedule);
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            catch (KeyNotFoundException)
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Create(DTO.ScheduleDto scheduleDto)
+        {
+            try
+            {
+                scheduleRepository.CreateSchedule(scheduleDto);
+                return Ok();
+            }
+            catch (KeyNotFoundException)
+            {
+                return NotFound();
+            }
+
+        }
+        [HttpPut]
+        public ActionResult Edit(DTO.ScheduleDto scheduleDto)
+        {
+            try
+            {
+                scheduleRepository.Update(scheduleDto);
+                return Ok();
+            }
+            catch (KeyNotFoundException)
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                scheduleRepository.Delete(id);
+                return Ok();
+            }
+            catch (KeyNotFoundException)
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
---
+++ AgendamentoHospital/DTO/ScheduleDto.cs
@@ -0,0 +1,13 @@
+﻿namespace AgendamentoHospital.DTO
+{
+    public class ScheduleDto
+    {
+        public int IdSchedule { get; set; }
+        public int IdHospital { get; set; }
+        public int IdSpecialty { get; set; }
+        public int IdProfessional { get; set; }
+        public DateTime DateHourSchedule { get; set; }
+        public int IdBeneficiary { get; set; }
+        public bool IsActiveSchedule { get; set; }
+    }
+}
